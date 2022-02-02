607e19b6dc3cf20edb7239bcb1320d1a1b6cbca9
mfquinonesc <manuelfqc@gmail.com>
2022-02-02 15:20:23 -0500

    Ejercicio 9

--- javascript/ejercicio9.js
+++ javascript/ejercicio9.js
@@ -1,1 +1,48 @@
 
+let btn = document.getElementById("btn");
+let content = document.getElementById("seld");
+let gend = document.getElementById("selg");
+let message = document.getElementById("mesg");
+
+
+llenarEdad();
+
+
+btn.onclick = function (){
+    let edad = content.selectedIndex + 0;
+    let genero = gend.selectedIndex + 0;
+    message.innerText= "";
+
+    if (edad>= 18){
+        if(genero ==2 && edad>= 25){
+            message.innerText= "Usted debe pagar $25000!";
+        }
+        if(genero ==2 && edad< 25){
+            message.innerText= "Usted debe pagar $20000!";
+        }
+
+        if(genero ==1 && edad>= 24){
+            message.innerText= "Usted debe pagar $30000!";
+        }
+        if(genero ==1 && edad< 24){
+            message.innerText= "Usted debe pagar $25000!";
+        }
+    }
+    else{
+        message.innerText= "Usted es menor de edad!\nNo puede ingresar!";
+    }
+};
+
+
+function llenarEdad(){
+    let l =  100;    
+    for (let i =1; i<l; i++){
+        let selector =  document.createElement("option");
+        selector.value = i+0;
+        selector.innerText=i+0;
+        content.appendChild(selector);
+    } 
+}
+
+
+
